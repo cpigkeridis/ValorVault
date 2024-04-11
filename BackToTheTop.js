0a9298c050760d2ae3ac954c64283861a3722c8e
Constantinos Pigkerides <cp212144@students.euc.ac.cy>
2024-04-11 20:20:46 +0300

    Added BackToTheTop.js Added back to the top button in sidebar Fixed boarder styles for items Added back to the top button style

---
+++ BackToTheTop.js
@@ -0,0 +1,6 @@
+document.getElementById('back-to-top').addEventListener('click', function() {
+    window.scrollTo({
+        top: 0, // Scroll to the top of the page
+        behavior: 'smooth' // Smooth scroll
+    });
+});
